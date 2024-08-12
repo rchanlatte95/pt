f5dd485b857b69db78993bc5c7e5255eaa5eb513
rchanlatte95 <113424043+rchanlatte95@users.noreply.github.com>
2024-08-12 15:18:15 -0400

    Adding Vectors

---
+++ src/headers/rac-v2.hpp
@@ -0,0 +1,16 @@
+#pragma once
+#include "rac.hpp"
+
+namespace rac::mth
+{
+    class alignas(f32) mut_v2
+    {
+
+    };
+
+    typedef mut_v2* mut_v2_ptr;
+    typedef mut_v2& mut_v2_ref;
+    typedef const mut_v2 v2;
+    typedef const mut_v2* v2_ptr;
+    typedef const mut_v2& v2_ref;
+}
---
+++ src/headers/rac-v3.hpp
@@ -0,0 +1,16 @@
+#pragma once
+#include "rac.hpp"
+
+namespace rac::mth
+{
+    class alignas(f32) mut_v3
+    {
+
+    };
+
+    typedef mut_v3* mut_v3_ptr;
+    typedef mut_v3& mut_v3_ref;
+    typedef const mut_v3 v3;
+    typedef const mut_v3* v3_ptr;
+    typedef const mut_v3& v3_ref;
+}
