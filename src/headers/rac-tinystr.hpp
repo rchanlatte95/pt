4f6d7abe76933060b08513a8399180eb121aa030
rchanlatte95 <113424043+rchanlatte95@users.noreply.github.com>
2024-08-13 12:06:37 -0400

    Adding to tinystr
    Added new constructors
    Added Clear() function to clear out string
    Added Fill() function to be able to fill string with an arbitrary character

--- src/headers/rac-tinystr.hpp
+++ src/headers/rac-tinystr.hpp
@@ -4,11 +4,11 @@
 namespace rac::static_strings
 {
     // https://pvs-studio.com/en/blog/terms/6658/
-    u8 TARGET_TINYSTR_BYTE_SZ = 16;
-    u8 LEN_VARIABLE_BYTE_SZ = sizeof(u8);
+    u64 TARGET_TINYSTR_BYTE_SZ = 16;
+    u64 LEN_VARIABLE_BYTE_SZ = sizeof(u8);
 
-    u8 TINYSTR_CAP = TARGET_TINYSTR_BYTE_SZ - LEN_VARIABLE_BYTE_SZ;
-    u8 MAX_TINYSTR_LEN = TINYSTR_CAP - NULL_TERMINATOR_LEN;
+    u64 TINYSTR_CAP = TARGET_TINYSTR_BYTE_SZ - LEN_VARIABLE_BYTE_SZ;
+    u64 MAX_TINYSTR_LEN = TINYSTR_CAP - NULL_TERMINATOR_LEN;
 
     class alignas(TARGET_TINYSTR_BYTE_SZ) mut_tinystr
     {
@@ -19,8 +19,32 @@ namespace rac::static_strings
 
     public:
 
-        mut_tinystr() { memset(&Length, NULL_TERMINATOR, TINYSTR_CAP); }
+        static constexpr u8 ClampToLen(u64 inputLength)
+        {
+            return (u8)std::min(inputLength, MAX_TINYSTR_LEN);
+        }
 
+        mut_tinystr() { memset(&Length, NULL_TERMINATOR, TINYSTR_CAP); }
+        mut_tinystr(cstr str)
+        {
+            Length = strnlen_s(str, MAX_TINYSTR_LEN);
+            memcpy_s(characters, MAX_TINYSTR_LEN, str, Length);
+        }
+        mut_tinystr(cstr str, u64 strLen)
+        {
+            Length = ClampToLen(strLen);
+            memcpy_s(characters, MAX_TINYSTR_LEN, str, Length);
+        }
+
+        INLINE void Clear()
+        {
+            memset(&Length, NULL_TERMINATOR, TINYSTR_CAP);
+        }
+        INLINE void Fill(u8 charToFillWith, u64 count = MAX_TINYSTR_LEN)
+        {
+            Length = ClampToLen(count);
+            memset(characters, charToFillWith, Length);
+        }
     };
 
     typedef mut_tinystr* mut_tinystr_ptr;
