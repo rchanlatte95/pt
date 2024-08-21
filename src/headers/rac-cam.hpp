be645adc43131ca17790b135fc5606b4ca65903b
Ryan Chanlatte <113424043+rchanlatte95@users.noreply.github.com>
2024-08-20 22:51:21 -0400

    Filling in camera class

--- src/headers/rac-cam.hpp
+++ src/headers/rac-cam.hpp
@@ -16,16 +16,29 @@ namespace rac::gfx
     class mut_cam
     {
     public:
+        mut_f32 focal_len;
         mut_f32 aspect_ratio;
-        mut_v3 viewport_sz;
-        mut_v3 viewport_uv;
+        mut_v2 viewport;
+        mut_v3 viewport_u;
+        mut_v3 viewport_v;
         mut_v3 center;
 
         mut_cam()
         {
+            focal_len = 0.0f;
             aspect_ratio = 0.0f;
-            viewport_sz = v3::ZERO;
-            viewport_uv = v3::ZERO;
+            viewport = v2::ZERO;
+            viewport_u = v3::ZERO;
+            viewport_v = v3::ZERO;
+            center = v3::ZERO;
+        }
+        mut_cam(i32 view_width, i32 view_height)
+        {
+            focal_len = 1.0f;
+            aspect_ratio = f32(view_width) / f32(view_height);
+            viewport = v2(2.0f, 2.0f * aspect_ratio);
+            viewport_u = v3(viewport.x, 0.0f, 0.0f);
+            viewport_v = v3(0.0f, -viewport.y, 0.0f);
             center = v3::ZERO;
         }
 
