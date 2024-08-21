7b335311925e2ad80c286ccaabd54398cf475989
Ryan Chanlatte <113424043+rchanlatte95@users.noreply.github.com>
2024-08-20 22:20:06 -0400

    Finished basic ray impl

--- src/headers/rac-ray.hpp
+++ src/headers/rac-ray.hpp
@@ -18,45 +18,15 @@ namespace rac::mth
 
         mut_ray()
         {
-            origin = v3(0.0f);
-            direction = v3(0.0f);
+            origin = v3::ZERO;
+            direction = v3::ZERO;
         }
-        mut_ray(v3 v)
+        mut_ray(v3 origin_, v3 direction_)
         {
-            x = v;
-            y = v;
-            z = v;
+            origin = origin_;
+            direction = direction_;
         }
 
-
-        INLINE static_strings::mut_cachestr ToStr(u32 decimal_places = 4) const noexcept
-        {
-            u64 MAX = static_strings::MAX_CACHESTR_LEN;
-            static_strings::mut_cachestr res;
-            bool use_exp = Between(x, MAX_F32_FOR_FULL_REP) ||
-                            Between(y, MAX_F32_FOR_FULL_REP) ||
-                            Between(z, MAX_F32_FOR_FULL_REP);
-
-            switch (decimal_places)
-            {
-            case 0:
-                snprintf(res.ToCharPtr(), MAX, use_exp ? "(%.0e, %.0e, %.0e)" : "(%.0f, %.0f, %.0f)", x, y, z);
-                break;
-            case 1:
-                snprintf(res.ToCharPtr(), MAX, use_exp ? "(%.1e, %.1e, %.1e)" : "(%.1f, %.1f, %.1f)", x, y, z);
-                break;
-            case 2:
-                snprintf(res.ToCharPtr(), MAX, use_exp ? "(%.2e, %.2e, %.2e)" : "(%.2f, %.2f, %.2f)", x, y, z);
-                break;
-            case 3:
-                snprintf(res.ToCharPtr(), MAX, use_exp ? "(%.3e, %.3e, %.3e)" : "(%.3f, %.3f, %.3f)", x, y, z);
-                break;
-            default:
-                snprintf(res.ToCharPtr(), MAX, use_exp ? "(%.4e, %.4e, %.4e)" : "(%.4f, %.4f, %.4f)", x, y, z);
-                break;
-            }
-            res.Resize();
-            return res;
-        }
+        INLINE v3 at(f32 t) const noexcept { return origin + t * direction; }
     };
 }
