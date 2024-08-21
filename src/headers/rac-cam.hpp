2d6173ce5b95f66a620de03dd321d2ee83cf8610
Ryan Chanlatte <113424043+rchanlatte95@users.noreply.github.com>
2024-08-20 22:38:08 -0400

    Adding camera class

---
+++ src/headers/rac-cam.hpp
@@ -0,0 +1,18 @@
+#include "rac-mth.hpp"
+#include "rac-v3.hpp"
+
+namespace rac::gfx
+{
+    class mut_cam;
+    typedef mut_cam* mut_cam_ptr;
+    typedef mut_cam& mut_cam_ref;
+    typedef const mut_cam cam;
+    typedef const mut_cam* cam_ptr;
+    typedef const mut_cam& cam_ref;
+
+    class mut_cam
+    {
+    public:
+
+    };
+}
