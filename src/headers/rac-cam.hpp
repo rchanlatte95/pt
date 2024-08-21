b25f07b133ab9b2e42b2471f3f17aefc2d7e0487
Ryan Chanlatte <113424043+rchanlatte95@users.noreply.github.com>
2024-08-20 22:44:08 -0400

    Starting to impl camera class

--- src/headers/rac-cam.hpp
+++ src/headers/rac-cam.hpp
@@ -1,8 +1,11 @@
 #include "rac-mth.hpp"
+#include "rac-v2.hpp"
 #include "rac-v3.hpp"
 
 namespace rac::gfx
 {
+    using namespace mth;
+
     class mut_cam;
     typedef mut_cam* mut_cam_ptr;
     typedef mut_cam& mut_cam_ref;
@@ -13,6 +16,18 @@ namespace rac::gfx
     class mut_cam
     {
     public:
+        mut_f32 aspect_ratio;
+        mut_v3 viewport_sz;
+        mut_v3 viewport_uv;
+        mut_v3 center;
+
+        mut_cam()
+        {
+            aspect_ratio = 0.0f;
+            viewport_sz = v3::ZERO;
+            viewport_uv = v3::ZERO;
+            center = v3::ZERO;
+        }
 
     };
 }
