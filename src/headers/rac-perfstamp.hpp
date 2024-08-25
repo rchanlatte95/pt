19359f68561a08fc3fb4a415f02bb978735d22d8
Ryan Chanlatte <113424043+rchanlatte95@users.noreply.github.com>
2024-08-24 23:55:05 -0400

    Adding start and end functions

--- src/headers/rac-perfstamp.hpp
+++ src/headers/rac-perfstamp.hpp
@@ -4,16 +4,36 @@
 
 namespace rac::chronology
 {
-    class mut_PerfStamp;
-    typedef mut_PerfStamp* mut_PerfStamp_ptr;
-    typedef mut_PerfStamp& mut_PerfStamp_ref;
-    typedef const mut_PerfStamp PerfStamp;
-    typedef const mut_PerfStamp* PerfStamp_ptr;
-    typedef const mut_PerfStamp& PerfStamp_ref;
+    class mut_PerfSample;
+    typedef mut_PerfSample* mut_PerfSample_ptr;
+    typedef mut_PerfSample& mut_PerfSample_ref;
+    typedef const mut_PerfSample PerfSample;
+    typedef const mut_PerfSample* PerfSample_ptr;
+    typedef const mut_PerfSample& PerfSample_ref;
 
-    class mut_PerfStamp
+    class mut_PerfSample
     {
+        bool active;
+        mut_TimeStamp start_time;
+        mut_TimeStamp end_time;
+        mut_CountStamp start_cycles;
+        mut_CountStamp end_cycles;
+
     public:
 
+        INLINE void Start()
+        {
+            if (active) { return; }
+
+            active = true;
+            start_time = TimeStamp(Timer::Now());
+            start_cycles = CountStamp(Counter::Now());
+        }
+        INLINE void End()
+        {
+            end_cycles = CountStamp(Counter::Now());
+            end_time = TimeStamp(Timer::Now());
+            active = false;
+        }
     };
 }
