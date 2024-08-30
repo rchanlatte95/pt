8fd3e7fa633a6fd62e7d2cd96088e3184da29ee1
Ryan Chanlatte <113424043+rchanlatte95@users.noreply.github.com>
2024-08-29 21:35:13 -0400

    Fixed byte size bug

--- src/headers/rac-packedtypes.hpp
+++ src/headers/rac-packedtypes.hpp
@@ -115,10 +115,10 @@ namespace rac
     {
         union
         {
-            p32 packed32[sizeof(f32) / sizeof(f64)];
+            p32 packed32[sizeof(p64) / sizeof(p32)];
             p64 packed64;
 
-            mut_f32 float32[sizeof(f32) / sizeof(f64)];
+            mut_f32 float32[sizeof(p64) / sizeof(f32)];
             mut_f64 float64;
         };
     };
@@ -127,21 +127,17 @@ namespace rac
     {
         union
         {
-            mut_i8 bytes[sizeof(__m128i)];
-            mut_i16 int16[sizeof(__m128i) / sizeof(u16)];
-            mut_i32 int32[sizeof(__m128i) / sizeof(u32)];
-            mut_i64 int64[sizeof(__m128i) / sizeof(u64)];
+            p32 packed32[sizeof(__m128) / sizeof(p32)];
+            p64 packed64[sizeof(__m128) / sizeof(p64)];
+            p128 packed128;
 
-            mut_u8 ubytes[sizeof(__m128i)];
-            mut_u16 uint16[sizeof(__m128i) / sizeof(u16)];
-            mut_u32 uint32[sizeof(__m128i) / sizeof(u32)];
-            mut_u64 uint64[sizeof(__m128i) / sizeof(u64)];
+            pf32 packedf32[sizeof(__m128) / sizeof(pf32)];
+            pf64 packedf64[sizeof(__m128) / sizeof(pf64)];
 
-            __m128i simd128;
+            mut_f32 float32[sizeof(__m128) / sizeof(f32)];
+            mut_f64 float64[sizeof(__m128) / sizeof(f64)];
 
-            p16 packed16[sizeof(__m128i) / sizeof(p16)];
-            p32 packed32[sizeof(__m128i) / sizeof(p32)];
-            p64 packed64[sizeof(__m128i) / sizeof(p64)];
+            __m128 f128;
         };
     };
 
@@ -149,23 +145,19 @@ namespace rac
     {
         union
         {
-            mut_i8 bytes[sizeof(__m256i)];
-            mut_i16 int16[sizeof(__m256i) / sizeof(u16)];
-            mut_i32 int32[sizeof(__m256i) / sizeof(u32)];
-            mut_i64 int64[sizeof(__m256i) / sizeof(u64)];
+            p32 packed32[sizeof(__m256) / sizeof(p32)];
+            p64 packed64[sizeof(__m256) / sizeof(p64)];
+            p128 packed128[sizeof(__m256) / sizeof(p128)];
 
-            mut_u8 ubytes[sizeof(__m256i)];
-            mut_u16 uint16[sizeof(__m256i) / sizeof(u16)];
-            mut_u32 uint32[sizeof(__m256i) / sizeof(u32)];
-            mut_u64 uint64[sizeof(__m256i) / sizeof(u64)];
+            pf32 packedf32[sizeof(__m256) / sizeof(pf32)];
+            pf64 packedf64[sizeof(__m256) / sizeof(pf64)];
+            pf128 packedf128[sizeof(__m256) / sizeof(pf128)];
 
-            __m128i simd128[sizeof(__m256i) / sizeof(__m128i)];
-            __m256i simd256;
+            mut_f32 float32[sizeof(__m256) / sizeof(f32)];
+            mut_f64 float64[sizeof(__m256) / sizeof(f64)];
 
-            p16 packed16[sizeof(__m256i) / sizeof(p16)];
-            p32 packed32[sizeof(__m256i) / sizeof(p32)];
-            p64 packed64[sizeof(__m256i) / sizeof(p64)];
-            p64 packed128[sizeof(__m256i) / sizeof(p128)];
+            __m128 f128[sizeof(__m256) / sizeof(__m128)];
+            __m256 f256;
         };
     };
 }
