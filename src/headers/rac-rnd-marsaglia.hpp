5e7ad9dd32fca8f711426e0fb8950b838feccd2f
Ryan Chanlatte <113424043+rchanlatte95@users.noreply.github.com>
2024-09-14 01:26:15 -0400

    Starting to impl XorShift RNG

--- src/headers/rac-rnd-marsaglia.hpp
+++ src/headers/rac-rnd-marsaglia.hpp
@@ -2,5 +2,10 @@
 
 namespace rac::rnd::marsaglia
 {
-
+    class mut_XorRng;
+    typedef mut_XorRng* mut_XorRng_ptr;
+    typedef mut_XorRng& mut_XorRng_ref;
+    typedef const mut_XorRng XorRng;
+    typedef const mut_XorRng* XorRng_ref;
+    typedef const mut_XorRng& XorRng_ref;
 }
