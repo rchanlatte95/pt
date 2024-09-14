5ca87a8737354ef89424da1031c0757afe009e19
Ryan Chanlatte <113424043+rchanlatte95@users.noreply.github.com>
2024-09-14 19:15:50 -0400

    Added 64 bit random numbers

--- src/headers/rac-rnd-marsaglia.hpp
+++ src/headers/rac-rnd-marsaglia.hpp
@@ -11,12 +11,21 @@ namespace rac::rnd::marsaglia
 
     // NOTE(RYAN_2024-09-14): Cryptographically generated random
     // numbers being used to select super primes
-    // (https://en.wikipedia.org/wiki/Super-prime).
+    // (https://en.wikipedia.org/wiki/Super-prime):
     // 41, 57, 190, 161, 127, 80, 98, 18, 187, 141, 124, 169, 75, 129, 180, 160
     u64 MAX_SEED_CT = 16;
     static u32 seeds[MAX_SEED_CT] = { 1063, 1723, 9293, 7481, 5381, 2803, 3733, 283, 8999, 6229, 5107, 8011, 2609, 5503, 8581, 7417 };
     static mut_u32 u32_seed = seeds[std::rand() % MAX_SEED_CT];
     static mut_i32 i32_seed = seeds[std::rand() % MAX_SEED_CT];
+
+    // NOTE(RYAN_2024-09-14): Cryptographically generated random
+    // numbers being used to select MORE super primes
+    // (https://en.wikipedia.org/wiki/Super-prime):
+    // 155, 143, 135, 85, 193, 199, 111, 192, 2, 140, 46, 146, 6, 15, 191, 188, 151, 26
+    // 135, 85, 193, 199, 111, 192, 2, 140, 46, 146, 6, 15, 191, 188, 151, 26
+    static u64 seeds[MAX_SEED_CT] = { 7057, 6323, };
+    static mut_u64 u32_seed = seeds[std::rand() % MAX_SEED_CT];
+    static mut_u64 i32_seed = seeds[std::rand() % MAX_SEED_CT];
 
     class mut_XorRng
     {
