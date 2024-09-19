4677acfe01fd00ca7aaf6aa14ebdf09193a8146d
rchanlatte95 <113424043+rchanlatte95@users.noreply.github.com>
2024-09-19 12:28:51 -0400

    Trying to fix issues

--- src/headers/rac-rnd-splitmix.hpp
+++ src/headers/rac-rnd-splitmix.hpp
@@ -13,9 +13,9 @@ namespace rac::rnd::SplitMix
     // NOTE(RYAN_2024-09-14): First 128 Super primes
     // (https://en.wikipedia.org/wiki/Super-prime):
     static u64 MAX_SEED_CT = 128;
-    static mut_u32 seeds[MAX_SEED_CT] = { 3, 5, 11, 17, 31, 41, 59, 67, 83, 109, 127, 157, 179, 191, 211, 241, 277, 283, 331, 353, 367, 401, 431, 461, 509, 547, 563, 587, 599, 617, 709, 739, 773, 797, 859, 877, 919, 967, 991, 1031, 1063, 1087, 1153, 1171, 1201, 1217, 1297, 1409, 1433, 1447, 1471, 1499, 1523, 1597, 1621, 1669, 1723, 1741, 1787, 1823, 1847, 1913, 2027, 2063, 2081, 2099, 2221, 2269, 2341, 2351, 2381, 2417, 2477, 2549, 2609, 2647, 2683, 2719, 2749, 2803, 2897, 2909, 3001, 3019, 3067, 3109, 3169, 3229, 3259, 3299, 3319, 3407, 3469, 3517, 3559, 3593, 3637, 3733, 3761, 3911, 3943, 4027, 4091, 4133, 4153, 4217, 4273, 4339, 4397, 4421, 4463, 4517, 4549, 4567, 4663, 4759, 4787, 4801, 4877, 4933, 4943, 5021, 5059, 5107, 5189, 5281, 5381, 5441 };
-    static mut_u32ptr SEEDS_BEGIN = seeds;
-    static mut_u32ptr SEEDS_END = seeds + MAX_SEED_CT;
+    static mut_u64 seeds[MAX_SEED_CT] = { 3, 5, 11, 17, 31, 41, 59, 67, 83, 109, 127, 157, 179, 191, 211, 241, 277, 283, 331, 353, 367, 401, 431, 461, 509, 547, 563, 587, 599, 617, 709, 739, 773, 797, 859, 877, 919, 967, 991, 1031, 1063, 1087, 1153, 1171, 1201, 1217, 1297, 1409, 1433, 1447, 1471, 1499, 1523, 1597, 1621, 1669, 1723, 1741, 1787, 1823, 1847, 1913, 2027, 2063, 2081, 2099, 2221, 2269, 2341, 2351, 2381, 2417, 2477, 2549, 2609, 2647, 2683, 2719, 2749, 2803, 2897, 2909, 3001, 3019, 3067, 3109, 3169, 3229, 3259, 3299, 3319, 3407, 3469, 3517, 3559, 3593, 3637, 3733, 3761, 3911, 3943, 4027, 4091, 4133, 4153, 4217, 4273, 4339, 4397, 4421, 4463, 4517, 4549, 4567, 4663, 4759, 4787, 4801, 4877, 4933, 4943, 5021, 5059, 5107, 5189, 5281, 5381, 5441 };
+    static mut_u64ptr SEEDS_BEGIN = seeds;
+    static mut_u64ptr SEEDS_END = seeds + MAX_SEED_CT;
 
     static mut_u64 splitmix_state;
 
@@ -31,18 +31,17 @@ namespace rac::rnd::SplitMix
             std::shuffle(SEEDS_BEGIN, SEEDS_END, generator);
             splitmix_state = seeds[distr(generator)];
         }
-
-        MAY_INLINE static void Init(u32 input_seed)
+        MAY_INLINE static void Init(u64 input_seed)
         {
             std::random_device rnd_dev;
             std::mt19937 rnd_gen(rnd_dev());
             std::uniform_int_distribution<int> distr(0, MAX_SEED_CT);
             std::shuffle(SEEDS_BEGIN, SEEDS_END, rnd_gen);
 
-            mut_u32 transformed_seed = input_seed ^ seeds[distr(rnd_gen)];
-            transformed_seed ^= transformed_seed << 9;
-            transformed_seed ^= transformed_seed >> 17;
-            transformed_seed ^= transformed_seed << 6;
+            mut_u64 transformed_seed = input_seed ^ seeds[distr(rnd_gen)];
+            transformed_seed ^= transformed_seed << 20;
+            transformed_seed ^= transformed_seed >> 41;
+            transformed_seed ^= transformed_seed << 5;
             splitmix_state = transformed_seed;
         }
 
