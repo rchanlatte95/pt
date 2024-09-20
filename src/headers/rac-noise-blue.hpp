06ab6e3b398ddeb23c6c1de67ab121bab5acc56e
rchanlatte95 <113424043+rchanlatte95@users.noreply.github.com>
2024-09-20 12:26:44 -0400

    Added blue noise decl

--- src/headers/rac-noise-blue.hpp
+++ src/headers/rac-noise-blue.hpp
@@ -1,6 +1,11 @@
-#include "rac.hpp"
+#include "rac-rnd-xorshiftroatate.hpp"
 
 namespace rac::noise::blue
 {
-
+    class mut_BlueNoise;
+    typedef mut_BlueNoise* mut_BlueNoise_ptr;
+    typedef mut_BlueNoise& mut_BlueNoise_ref;
+    typedef const mut_BlueNoise BlueNoise;
+    typedef const mut_BlueNoise* BlueNoise_ptr;
+    typedef const mut_BlueNoise& BlueNoise_ref;
 }
