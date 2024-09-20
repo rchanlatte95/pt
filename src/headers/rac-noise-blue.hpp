8f751120546cd939b9fbf814ebcee0b2f3e4adb7
rchanlatte95 <113424043+rchanlatte95@users.noreply.github.com>
2024-09-20 13:41:14 -0400

    added class impl for blue noise

--- src/headers/rac-noise-blue.hpp
+++ src/headers/rac-noise-blue.hpp
@@ -8,4 +8,11 @@ namespace rac::noise::blue
     typedef const mut_BlueNoise BlueNoise;
     typedef const mut_BlueNoise* BlueNoise_ptr;
     typedef const mut_BlueNoise& BlueNoise_ref;
+
+    u64 SAMPLE_CT = 32;
+
+    class mut_BlueNoise
+    {
+    public:
+    };
 }
