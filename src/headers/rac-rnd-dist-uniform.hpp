1fe19be3117f6d69e6209706322d85f47614b19c
Ryan Chanlatte <113424043+rchanlatte95@users.noreply.github.com>
2024-10-13 23:08:39 -0400

    Setting up scratch memory

--- src/headers/rac-rnd-dist-uniform.hpp
+++ src/headers/rac-rnd-dist-uniform.hpp
@@ -1,5 +1,6 @@
 #pragma once
 #include "rac-packedtypes.hpp"
+#include "rac-rnd-xorshiftroatate.hpp"
 
 namespace rac::rnd::distribution::uniform
 {
@@ -10,12 +11,30 @@ namespace rac::rnd::distribution::uniform
     typedef const mut_UniformDist* UniformDist_ptr;
     typedef const mut_UniformDist& UniformDist_ref;
 
+    static std::vector<f64> scratch;
     class mut_UniformDist
     {
+    private:
+
+        MAY_INLINE static void FillScratch(u64 scratch_len)
+        {
+            if (scratch.capacity() >= scratch_len)
+            {
+                scratch.reserve(scratch_len);
+            }
+
+            for (int i = 0; i < scratch_len; ++i)
+            {
+                //scratch[i] = ;
+            }
+        }
+
     public:
 
-        MAY_INLINE static std::vector<f32> Convert(u64 len, u64 X_MAX)
+        MAY_INLINE static std::vector<f32> Convert(u32 len, u64 X_MAX)
         {
+            FillScratch((u64)len * 8);
+
             std::vector<f32> res;
             res.reserve(len);
 
