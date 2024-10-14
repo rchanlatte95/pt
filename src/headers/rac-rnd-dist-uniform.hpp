726c1de8a673ddfdbe1a36361a31a90d8c9e7a9e
Ryan Chanlatte <113424043+rchanlatte95@users.noreply.github.com>
2024-10-14 00:05:39 -0400

    Impl normal distribution algo

--- src/headers/rac-rnd-dist-uniform.hpp
+++ src/headers/rac-rnd-dist-uniform.hpp
@@ -15,39 +15,15 @@ namespace rac::rnd::distribution::uniform
     typedef const mut_UniformDist* UniformDist_ptr;
     typedef const mut_UniformDist& UniformDist_ref;
 
-    static std::vector<mut_v2> scratch;
     static f32 SIGN_HALF_LN_2PI = -HALF_LN_2PI;
     static f32 F32_MAX_X = 4.60303963306f; // √(2k + 10 * ln(10))
     class mut_UniformDist
     {
-    private:
-
-        MAY_INLINE static void FillF32Scratch(u64 scratch_len)
-        {
-            using namespace rac::rnd::XorShiftRotate;
-            if (scratch.capacity() < scratch_len)
-            {
-                scratch.reserve(scratch_len);
-            }
-
-            f32 X_MAX = F32_MAX_X;
-            f32 X_MIN = -X_MAX;
-            for (int i = 0; i < scratch_len; ++i)
-            {
-                f32 x = XsrRng::GetF32(X_MIN, X_MAX);
-                f32 y = XsrRng::GetF32();
-                scratch[i] = v2(x, y);
-            }
-        }
-
-        // Check if value is inside
-        INLINE bool Normal_Helper_(v2_ref v) { return v.y <= Get(v.x); }
-
     public:
 
         // Get f(x) where f(x) is a non-parameterized gaussian
         // distribution function.
-        INLINE f32 Get(f32 x)
+        INLINE static f32 Get(f32 x)
         {
             f32 half_x2 = x * x * 0.5f;
             return std::expf(SIGN_HALF_LN_2PI - half_x2);
@@ -55,7 +31,7 @@ namespace rac::rnd::distribution::uniform
 
         // Check if value is possibly a non-parameterized gaussian
         // distribution function.
-        INLINE bool Normal(f32 input_x, f32 input_y)
+        INLINE static bool Normal(f32 input_x, f32 input_y)
         {
             const bool x_in_range = input_x >= F32_EPSILON && input_x <= F32_MAX_X;
             return x_in_range && input_y <= Get(input_x);
@@ -63,20 +39,33 @@ namespace rac::rnd::distribution::uniform
 
         // Check if value is possibly a non-parameterized gaussian
         // distribution function.
-        INLINE bool Normal(v2_ref v)
+        INLINE static bool Normal(v2_ref v)
         {
             const bool x_in_range = v.x >= F32_EPSILON && v.x <= F32_MAX_X;
             return x_in_range && v.y <= Get(v.x);
         }
 
-        MAY_INLINE static std::vector<f32> Convert(u32 len)
+        MAY_INLINE static std::vector<mut_v2> Convert(u32 len)
         {
-            u64 SCRATCH_CT = (u64)len * 2ULL;
-            FillF32Scratch(SCRATCH_CT);
+            using namespace rac::rnd::XorShiftRotate;
 
-            std::vector<f32> res;
+            std::vector<mut_v2> res;
             res.reserve(len);
 
+            f32 X_MAX = F32_MAX_X;
+            f32 X_MIN = -X_MAX;
+            mut_f32 y = 0.0f;
+            mut_i32 i = 0;
+            while(i < len)
+            {
+                f32 x = XsrRng::GetF32(X_MIN, X_MAX);
+                y = XsrRng::GetF32();
+                if (Normal(x, y))
+                {
+                    res.push_back(v2(x, y));
+                    ++i;
+                }
+            }
             return res;
         }
     };
