3c56dda524d050a0675b31e8c620c86ed1b93dbc
Ryan Chanlatte <113424043+rchanlatte95@users.noreply.github.com>
2024-11-24 22:02:45 -0500

    breaking headers down

--- src/headers/rac-color.hpp
+++ src/headers/rac-color.hpp
@@ -106,138 +106,30 @@ namespace rac::gfx
         static u8 BITS_PER_COLOR_COMPONENT = sizeof(r) * BITS_IN_BYTE;
         static u8 BITS_PER_PIXEL = BITS_PER_COLOR_COMPONENT * 4;
 
-        mut_Color()
-        {
-            b = 0;
-            g = 0;
-            r = 0;
-            opacity = 255;
-        }
-        mut_Color(u8 _r, u8 _g, u8 _b, u8 _a = 255)
-        {
-            b = _b;
-            g = _g;
-            r = _r;
-            opacity = _a;
-        }
-        mut_Color(f32 _r, f32 _g, f32 _b, f32 _a)
-        {
-            b = (u8)(_b * 255.999f);
-            g = (u8)(_g * 255.999f);
-            r = (u8)(_r * 255.999f);
-            opacity = (u8)(_a * 255.999f);
-        }
-        mut_Color(u32 color_code)
-        {
-            r = (u8)(color_code >> 24);
-            g = (u8)((color_code & 0x00FF0000) >> 16);
-            b = (u8)((color_code & 0x0000FF00) >> 8);
-            opacity = (u8)(color_code & 0x000000FF);
-        }
-        mut_Color(i32 color_code)
-        {
-            r = (u8)(color_code >> 24);
-            g = (u8)((color_code & 0x00FF0000) >> 16);
-            b = (u8)((color_code & 0x0000FF00) >> 8);
-            opacity = (u8)(color_code & 0x000000FF);
-        }
-        mut_Color(v3_ref v, u8 _a = 255)
-        {
-            v3 norm_v = (v.Norm() + v3::ONE) * 0.5f;
-            r = (u8)(norm_v.x * 255.999f);
-            g = (u8)(norm_v.y * 255.999f);
-            b = (u8)(norm_v.z * 255.999f);
-            opacity = _a;
-        }
+        mut_Color();
+        mut_Color(u8 _r, u8 _g, u8 _b, u8 _a = 255);
+        mut_Color(f32 _r, f32 _g, f32 _b, f32 _a);
+        mut_Color(u32 color_code);
+        mut_Color(i32 color_code);
+        mut_Color(v3_ref v, u8 _a = 255);
 
         // Linearly interpolate from one color to another based on a
-        MAY_INLINE static Color Mix(Color from, Color to, f32 f)
-        {
-            f32 from_rf = from.r;
-            f32 from_gf = from.b;
-            f32 from_bf = from.b;
-            f32 from_a = from.opacity;
+        MAY_INLINE static Color Mix(Color from, Color to, f32 f);
+        INLINE f32 LinearToGamma(f32 linear_color_component) const noexcept;
+        INLINE f32 GammaToLinear(f32 gamma_color_component) const noexcept;
+        INLINE Color Luminance() const noexcept;
 
-            f32 to_rf = to.r;
-            f32 to_gf = to.b;
-            f32 to_bf = to.b;
-            f32 to_a = to.opacity;
+        INLINE u32 GetU32() const noexcept;
+        INLINE operator u32() const noexcept;
+        INLINE i32 GetI32() const noexcept;
+        INLINE operator i32() const noexcept;
 
-            f32 one_minus_a = 1.0f - f;
-            u8 new_r = (u8)(from_rf * one_minus_a + to_rf * f);
-            u8 new_g = (u8)(from_gf * one_minus_a + to_gf * f);
-            u8 new_b = (u8)(from_bf * one_minus_a + to_bf * f);
-            u8 new_a = (u8)(from_a * one_minus_a + to_a * f);
-            return Color(new_r, new_g, new_b, new_a);
-        }
+        INLINE Color_ref operator=(Color_ref rhs) noexcept;
+        INLINE Color_ref operator=(u32 color_code) noexcept;
+        INLINE Color_ref operator=(i32 color_code) noexcept;
 
-        INLINE f32 LinearToGamma(f32 linear_color_component) const noexcept
-        {
-            return powf((f32)linear_color_component * INV_U8_MAX, INV_GAMMA);
-        }
-        INLINE f32 GammaToLinear(f32 gamma_color_component) const noexcept
-        {
-            return powf((f32)gamma_color_component * INV_U8_MAX, GAMMA);
-        }
-
-        INLINE Color Luminance() const noexcept
-        {
-            f32 rY = LUMA_REC709_R * LinearToGamma(r);
-            f32 gY = LUMA_REC709_G * LinearToGamma(g);
-            f32 bY = LUMA_REC709_B * LinearToGamma(b);
-            u8 new_r = (u8)(rY * 255.999f);
-            u8 new_g = (u8)(gY * 255.999f);
-            u8 new_b = (u8)(bY * 255.999f);
-            return Color(new_r, new_g, new_b, opacity);
-        }
-
-        INLINE u32 GetU32() const noexcept { return *(u32ptr(&r)); }
-        INLINE operator u32() const noexcept { return GetU32(); }
-
-        INLINE i32 GetI32() const noexcept { return *(i32ptr(&r)); }
-        INLINE operator i32() const noexcept { return GetI32(); }
-
-        INLINE Color_ref operator=(Color_ref rhs) noexcept
-        {
-            b = rhs.b;
-            g = rhs.g;
-            r = rhs.r;
-            opacity = rhs.opacity;
-            return *this;
-        }
-        INLINE Color_ref operator=(u32 color_code) noexcept
-        {
-            r = (u8)(color_code >> 24);
-            g = (u8)((color_code & 0x00FF0000) >> 16);
-            b = (u8)((color_code & 0x0000FF00) >> 8);
-            opacity = (u8)(color_code & 0x000000FF);
-            return *this;
-        }
-        INLINE Color_ref operator=(i32 color_code) noexcept
-        {
-            opacity = (u8)(color_code >> 24);
-            b = (u8)((color_code & 0x00FF0000) >> 16);
-            g = (u8)((color_code & 0x0000FF00) >> 8);
-            r = (u8)(color_code & 0x000000FF);
-            return *this;
-        }
-
-        INLINE static_strings::mut_cachestr ToStr() const noexcept
-        {
-            u64 MAX = static_strings::MAX_CACHESTR_LEN;
-            static_strings::mut_cachestr res;
-            snprintf(res.ToCharPtr(), MAX, "(%d, %d, %d, %d)", r, g, b, opacity);
-            res.Resize();
-            return res;
-        }
-        INLINE static_strings::mut_cachestr ToPpmStr() const noexcept
-        {
-            u64 MAX = static_strings::MAX_CACHESTR_LEN;
-            static_strings::mut_cachestr res;
-            snprintf(res.ToCharPtr(), MAX, "(%d, %d, %d)", r, g, b);
-            res.Resize();
-            return res;
-        }
+        INLINE static_strings::mut_cachestr ToStr() const noexcept;
+        INLINE static_strings::mut_cachestr ToPpmStr() const noexcept;
     };
 
     class mut_Colorf
@@ -252,187 +144,29 @@ namespace rac::gfx
         static u8 BITS_PER_COLOR_COMPONENT = sizeof(r) * BITS_IN_BYTE;
         static u8 BITS_PER_PIXEL = BITS_PER_COLOR_COMPONENT * 4;
 
-        mut_Colorf()
-        {
-            r = 0.0f;
-            g = 0.0f;
-            b = 0.0f;
-            opacity = 1.0f;
-        }
-        mut_Colorf(u8 _r, u8 _g, u8 _b, u8 _a)
-        {
-            r = _r * INV_U8_MAX;
-            g = _g * INV_U8_MAX;
-            b = _b * INV_U8_MAX;
-            opacity = _a * INV_U8_MAX;
-        }
-        mut_Colorf(f32 _r, f32 _g, f32 _b, f32 _a = 1.0f)
-        {
-            r = _r;
-            g = _g;
-            b = _b;
-            opacity = _a;
-        }
-        mut_Colorf(Color_ref c)
-        {
-            r = c.r * INV_U8_MAX;
-            g = c.g * INV_U8_MAX;
-            b = c.b * INV_U8_MAX;
-            opacity = c.opacity * INV_U8_MAX;
-        }
-        mut_Colorf(v3_ref v, f32 _a = 1.0f)
-        {
-            v3 norm_v = (v.Norm() + v3::ONE) * 0.5f;
-            r = norm_v.x;
-            g = norm_v.y;
-            b = norm_v.z;
-            opacity = _a;
-        }
+        mut_Colorf();
+        mut_Colorf(u8 _r, u8 _g, u8 _b, u8 _a);
+        mut_Colorf(f32 _r, f32 _g, f32 _b, f32 _a = 1.0f);
+        mut_Colorf(Color_ref c);
+        mut_Colorf(v3_ref v, f32 _a = 1.0f);
 
         // Linearly interpolate from one color to another based on a
-        MAY_INLINE static Colorf Mix(Colorf from, Colorf to, f32 a)
-        {
-            f32 one_minus_a = 1.0f - a;
-            f32 new_r = from.r * one_minus_a + to.r * a;
-            f32 new_g = from.g * one_minus_a + to.g * a;
-            f32 new_b = from.b * one_minus_a + to.b * a;
-            f32 new_a = from.opacity * one_minus_a + to.opacity * a;
-            return Colorf(new_r, new_g, new_b, new_a);
-        }
+        MAY_INLINE static Colorf Mix(Colorf from, Colorf to, f32 a);
+        INLINE f32 LinearToGamma(f32 linear_color_component) const noexcept;
+        INLINE f32 GammaToLinear(f32 gamma_color_component) const noexcept;
+        INLINE Colorf ToLinear() const noexcept;
+        INLINE Colorf ToGamma() const noexcept;
 
-        INLINE f32 LinearToGamma(f32 linear_color_component) const noexcept
-        {
-            return powf(linear_color_component, INV_GAMMA);
-        }
-        INLINE f32 GammaToLinear(f32 gamma_color_component) const noexcept
-        {
-            return powf(gamma_color_component, GAMMA);
-        }
-        INLINE Colorf ToLinear() const noexcept
-        {
-            return Colorf(LinearToGamma(r), LinearToGamma(g), LinearToGamma(b), opacity);
-        }
-        INLINE Colorf ToGamma() const noexcept
-        {
-            return Colorf(GammaToLinear(r), GammaToLinear(g), GammaToLinear(b), opacity);
-        }
+        INLINE Colorf Luminance() const noexcept;
 
-        INLINE Colorf Luminance() const noexcept
-        {
-            f32 rY = LUMA_REC709_R * LinearToGamma(r);
-            f32 gY = LUMA_REC709_G * LinearToGamma(g);
-            f32 bY = LUMA_REC709_B * LinearToGamma(b);
-            return Colorf(rY, gY, bY, opacity);
-        }
+        INLINE Color ToColor() const noexcept;
+        INLINE operator Color() const noexcept;
 
-        INLINE Color ToColor() const noexcept { return Color(*this); }
-        INLINE operator Color() const noexcept { return ToColor(); }
+        INLINE Colorf_ref operator=(Colorf_ref rhs) noexcept;
 
-        INLINE Colorf_ref operator=(Colorf_ref rhs) noexcept
-        {
-            b = rhs.b;
-            g = rhs.g;
-            r = rhs.r;
-            opacity = rhs.opacity;
-            return *this;
-        }
+        mut_f32 operator [] (i32 i) const;
+        mut_f32ref operator [] (i32 i);
 
-        mut_f32 operator [] (i32 i) const { return *(&r + i); }
-        mut_f32ref operator [] (i32 i) { return *(&r + i); }
-
-        INLINE static_strings::mut_cachestr ToStr(u32 decimal_places = 4) const noexcept
-        {
-            u64 MAX = static_strings::MAX_CACHESTR_LEN;
-            static_strings::mut_cachestr res;
-
-            switch (decimal_places)
-            {
-            case 0:
-                snprintf(res.ToCharPtr(), MAX, "(%.0f, %.0f, %.0f, %.0f)", r, g, b, opacity);
-                break;
-            case 1:
-                snprintf(res.ToCharPtr(), MAX, "(%.1f, %.1f, %.1f, %.1f)", r, g, b, opacity);
-                break;
-            case 2:
-                snprintf(res.ToCharPtr(), MAX, "(%.2f, %.2f, %.2f, %.2f)", r, g, b, opacity);
-                break;
-            case 3:
-                snprintf(res.ToCharPtr(), MAX, "(%.3f, %.3f, %.3f, %.3f)", r, g, b, opacity);
-                break;
-            default:
-                snprintf(res.ToCharPtr(), MAX, "(%.4f, %.4f, %.4f, %.4f)", r, g, b, opacity);
-                break;
-            }
-            res.Resize();
-            return res;
-        }
+        INLINE static_strings::mut_cachestr ToStr(u32 decimal_places = 4) const noexcept;
     };
-
-    // primary colors
-    Color Color::BLACK = Color(0x000000ff);
-    Color Color::WHITE = Color(0xffffffff);
-    Color Color::RED =   Color(0xff0000ff);
-    Color Color::GREEN = Color(0x00ff00ff);
-    Color Color::BLUE =  Color(0x0000ffff);
-    Color Color::CYAN =  Color(0x00ffffff);
-    Color Color::MAGENTA =Color(0xff00ffff);
-    Color Color::YELLOW = Color(0xffff00ff);
-
-    // very common colors according to:
-    //      https://www.color-hex.com/color-names.html ,
-    //      https://simple.wikipedia.org/wiki/Colour ,
-    //      and random colors I liked
-    Color Color::DARK_GRAY = Color(0x262626ff);
-    Color Color::GRAY = Color(0x7f7f7fff);
-    Color Color::LIGHT_GRAY = Color(0xd9d9d9ff);
-    Color Color::ORANGE = Color(0xffa500ff);
-    Color Color::PURPLE = Color(0xa020f0ff);
-    Color Color::LAVENDER = Color(0xb57edcff);
-    Color Color::IVORY = Color(0xfffff0ff);
-    Color Color::BURGUNDY = Color(0x800020ff);
-    Color Color::SKY_BLUE = Color(0x00ccffff);
-    Color Color::OLIVE = Color(0x808000ff);
-    Color Color::FOREST_GREEN = Color(0x228b22ff);
-    Color Color::OXBLOOD = Color(0x4a0000ff);
-    Color Color::OXFORD_BLUE = Color(0x002147ff);
-    Color Color::TURQUOISE = Color(0x40e0d0ff);
-    Color Color::CHARTREUSE = Color(0x76ee00ff);
-    Color Color::SALMON = Color(0xfa8072ff);
-    Color Color::BROWN = Color(0x654321ff);
-    Color Color::RUST = Color(0xb7410eff);
-    Color Color::TEAL = Color(0x008080ff);
-    Color Color::COBALT = Color(0x0047abff);
-    Color Color::EGGPLANT = Color(0x614051ff);
-    Color Color::PINK = Color(0xfc8eacff);
-    Color Color::KEY_LIME = Color(0xe8f48cff);
-    Color Color::LILAC = Color(0xc8a2c8ff);
-    Color Color::MAROON = Color(0xb03060ff);
-    Color Color::BEIGE = Color(0xf5f5dcff);
-    Color Color::TAN = Color(0xd2b48cff);
-    Color Color::PEACH = Color(0xffe5b4ff);
-    Color Color::LEMON = Color(0xfff700ff);
-    Color Color::LIME = Color(0x32cd32ff);
-    Color Color::NAVY_BLUE = Color(0x1974d2ff);
-    Color Color::MISALI = Color(0x80c0ffff);
-
-    // Pokemon Colors
-    Color Color::GOLD = Color(0xffd700ff);
-    Color Color::SILVER = Color(0xc0c0c0ff);
-    Color Color::RUBY = Color(0xe0115fff);
-    Color Color::SAPPHIRE = Color(0x0f52baff);
-    Color Color::EMERALD = Color(0x50c878ff);
-    Color Color::DIAMOND = Color(0xb9f2ffff);
-    Color Color::PEARL = Color(0xff5733ff);
-    Color Color::PLATINUM = Color(0xe5e4e2ff);
-    Color Color::CELADON = Color(0xace1afff);
-    Color Color::FUCHSIA = Color(0xfe4164ff);
-    Color Color::SAFFRON = Color(0xf4c430ff);
-    Color Color::CERULEAN = Color(0x007ba7ff);
-    Color Color::INDIGO = Color(0x4b0082ff);
-    Color Color::PEWTER = Color(0x8ba8b7ff);
-    Color Color::VIRIDIAN = Color(0x40826dff);
-    Color Color::GOLDENROD = Color(0xdaa520ff);
-    Color Color::MAHOGANY = Color(0xc04000ff);
-    Color Color::VIOLET = Color(0xee82eeff);
-    Color Color::CINNABAR = Color(0xe34234ff);
 }
---
+++ src/rac-v3.cpp
@@ -0,0 +1,182 @@
+#include "headers/rac-v3.hpp"
+
+namespace rac::mth
+{
+    v3::mut_v3()
+    {
+        x = 0.0f;
+        y = 0.0f;
+        z = 0.0f;
+    }
+    v3::mut_v3(f32 v)
+    {
+        x = v;
+        y = v;
+        z = v;
+    }
+    v3::mut_v3(f32 x_, f32 y_)
+    {
+        x = x_;
+        y = y_;
+        z = 0.0f;
+    }
+    v3::mut_v3(f32 x_, f32 y_, f32 z_)
+    {
+        x = x_;
+        y = y_;
+        z = z_;
+    }
+    v3::mut_v3(v2 v, f32 z_)
+    {
+        x = v.x;
+        y = v.y;
+        z = z_;
+    }
+
+    INLINE v3 v3::operator-() const noexcept { return v3(-x, -y, -z); }
+    INLINE f32 v3::operator[](i32 i) const noexcept { return *(&x + i); }
+    INLINE mut_f32ref v3::operator[](i32 i) { return *(&x + i); }
+
+    INLINE mut_v3_ref v3::operator=(v3_ref v_ref) noexcept
+    {
+        x = v_ref.x;
+        y = v_ref.y;
+        z = v_ref.z;
+        return *this;
+    }
+
+    INLINE mut_v3_ref v3::operator+=(v3_ref v) noexcept
+    {
+        x += v.x;
+        y += v.y;
+        z += v.z;
+        return *this;
+    }
+    INLINE mut_v3_ref v3::operator-=(v3_ref v) noexcept
+    {
+        x -= v.x;
+        y -= v.y;
+        z -= v.z;
+        return *this;
+    }
+    INLINE mut_v3_ref v3::operator*=(f32 v) noexcept
+    {
+        x *= v;
+        y *= v;
+        z *= v;
+        return *this;
+    }
+    INLINE mut_v3_ref v3::operator/=(f32 v) noexcept
+    {
+        return *this *= 1.0f / v;
+    }
+
+    INLINE f32 v3::SqrMag() const noexcept
+    {
+        return x * x + y * y + z * z;
+    }
+    INLINE f32 v3::InvSqrMag() const noexcept
+    {
+        return 1.0f / (x * x + y * y + z * z);
+    }
+    INLINE f32 v3::Mag() const noexcept
+    {
+        return sqrtf(this->SqrMag());
+    }
+    INLINE f32 v3::InvMag() const noexcept
+    {
+        return 1.0f / sqrtf(this->SqrMag());
+    }
+    INLINE v3 v3::Norm() const noexcept
+    {
+        f32 invMag = this->InvMag();
+        return v3(x * invMag, y * invMag, z * invMag);
+    }
+
+    INLINE pf128 v3::Pack128() const noexcept
+    {
+        return pf128(x, y, z, 0.0f);
+    }
+
+    INLINE static_strings::mut_cachestr v3::ToStr(u32 decimal_places) const noexcept
+    {
+        u64 MAX = static_strings::MAX_CACHESTR_LEN;
+        static_strings::mut_cachestr res;
+        bool use_exp = Between(x, MAX_F32_FOR_FULL_REP) ||
+            Between(y, MAX_F32_FOR_FULL_REP) ||
+            Between(z, MAX_F32_FOR_FULL_REP);
+
+        switch (decimal_places)
+        {
+        case 0:
+            snprintf(res.ToCharPtr(), MAX, use_exp ? "(%.0e, %.0e, %.0e)" : "(%.0f, %.0f, %.0f)", x, y, z);
+            break;
+        case 1:
+            snprintf(res.ToCharPtr(), MAX, use_exp ? "(%.1e, %.1e, %.1e)" : "(%.1f, %.1f, %.1f)", x, y, z);
+            break;
+        case 2:
+            snprintf(res.ToCharPtr(), MAX, use_exp ? "(%.2e, %.2e, %.2e)" : "(%.2f, %.2f, %.2f)", x, y, z);
+            break;
+        case 3:
+            snprintf(res.ToCharPtr(), MAX, use_exp ? "(%.3e, %.3e, %.3e)" : "(%.3f, %.3f, %.3f)", x, y, z);
+            break;
+        default:
+            snprintf(res.ToCharPtr(), MAX, use_exp ? "(%.4e, %.4e, %.4e)" : "(%.4f, %.4f, %.4f)", x, y, z);
+            break;
+        }
+        res.Resize();
+        return res;
+    }
+
+    INLINE v3 operator*(v3_ref u, f32 v)
+    {
+        return v3(u.x * v, u.y * v, u.z * v);
+    }
+    INLINE v3 operator/(v3_ref u, f32 v) { return u * (1.0f / v); }
+    INLINE v3 operator*(v3_ref u, i32 v)
+    {
+        return v3(u.x * (f32)v, u.y * (f32)v, u.z * (f32)v);
+    }
+    INLINE v3 operator/(v3_ref u, i32 v) { return u * (f32)(1.0f / (f32)v); }
+
+    INLINE v3 operator+(v3_ref u, v3_ref v)
+    {
+        return v3(u.x + v.x, u.y + v.y, u.z + v.z);
+    }
+    INLINE v3 operator-(v3_ref u, v3_ref v)
+    {
+        return v3(u.x - v.x, u.y - v.y, u.z - v.z);
+    }
+    INLINE v3 operator*(v3_ref u, v3_ref v)
+    {
+        return v3(u.x * v.x, u.y * v.y, u.z * v.z);
+    }
+    INLINE v3 operator/(v3_ref u, v3_ref v)
+    {
+        return v3(u.x / v.x, u.y / v.y, u.z / v.z);
+    }
+
+    INLINE v3 Norm(v3_ref v) noexcept { return v * v.InvMag(); }
+    INLINE f32 Dot(v3_ref u, v3_ref v) noexcept
+    {
+        return u.x * v.x + u.y * v.y + u.z * v.z;
+    }
+    INLINE v3 Cross(v3_ref u, v3_ref v) noexcept
+    {
+        return v3(u.y * v.z - u.z * v.y,
+            u.z * v.x - u.x * v.z,
+            u.x * v.y - u.y * v.x);
+    }
+
+    v3 v3::ZERO = v3(0.0f);
+
+    v3 v3::ONE = v3(1.0f);
+    v3 v3::SIGN_ONE = -ONE;
+
+    v3 v3::RIGHT = v3(1.0f, 0.0f, 0.0f);
+    v3 v3::UP = v3(0.0f, 1.0f, 0.0f);
+    v3 v3::FORWARD = v3(0.0f, 0.0f, 1.0f);
+    v3 v3::LEFT = -RIGHT;
+    v3 v3::DOWN = -UP;
+    v3 v3::BACKWARD = -FORWARD;
+}
